12850f97d13df6135420ef9622e7548cea214813
jjamby <valenciamajha@gmail.com>
2026-05-09 19:08:20 +0800

    feat: initial employee and schedule views

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue'
 import router from './router.js'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 
 createApp(App).use(router).mount('#app')
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -3,7 +3,9 @@ import MainLayout from './layouts/MainLayout.vue'
 import Login from './views/Login.vue'
 import Landing from './views/Landing.vue'
 import Dashboard from './views/Dashboard.vue'
-import Inventory from './views/Inventory.vue' 
+import Inventory from './views/Inventory.vue'
+import Employee from './views/Employee.vue'
+import Schedule from './views/Schedule.vue' 
 
 const routes = [
   {
@@ -16,32 +18,42 @@ const routes = [
     name: 'Landing',
     component: Landing
   },
-  {
-    path: '/',
-    component: MainLayout,
-    children: [
-      {
-        path: 'dashboard',
-        name: 'Dashboard',
-        component: Dashboard
-      },
-      {
-        path: 'inventory',
-        name: 'Inventory',
-        component: Inventory
-      },
-      {
-        path: 'search',
-        name: 'Search',
-        component: () => import('./views/Search.vue')
-      },
-      {
-        path: 'about',
-        name: 'About',
-        component: () => import('./views/About.vue')
-      },
-    ]
-  }
+    {
+      path: '/dashboard',
+      component: MainLayout,
+      children: [
+        {
+          path: '',
+          name: 'Dashboard',
+          component: Dashboard
+        },
+        {
+          path: 'employee',
+          name: 'Employee',
+          component: Employee
+        },
+        {
+          path: 'schedule',
+          name: 'Schedule',
+          component: Schedule
+        },
+        {
+          path: 'inventory',
+          name: 'Inventory',
+          component: Inventory
+        },
+        {
+          path: 'search',
+          name: 'Search',
+          component: () => import('./views/Search.vue')
+        },
+        {
+          path: 'about',
+          name: 'About',
+          component: () => import('./views/About.vue')
+        },
+      ]
+    }
 ]
 
 const router = createRouter({
